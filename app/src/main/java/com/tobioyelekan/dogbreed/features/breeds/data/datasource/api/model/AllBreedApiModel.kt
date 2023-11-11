dcbd68782dfe94be9f1f7b2a8adfc9f33fa33f45
Tobi <miketobi73@gmail.com>
2023-11-11 12:19:57 +0100

    rename AllBreed to AllBreedApiModel to clarify model type

--- app/src/main/java/com/tobioyelekan/dogbreed/features/breeds/data/datasource/api/model/AllBreedApiModel.kt
+++ app/src/main/java/com/tobioyelekan/dogbreed/features/breeds/data/datasource/api/model/AllBreedApiModel.kt
@@ -3,6 +3,6 @@ package com.tobioyelekan.dogbreed.features.breeds.data.datasource.api.model
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class AllBreed(
+data class AllBreedApiModel(
     val breed: Map<String, List<String>>
 )
--- app/src/main/java/com/tobioyelekan/dogbreed/features/breeds/data/datasource/api/response/GetAllBreedResponse.kt
+++ app/src/main/java/com/tobioyelekan/dogbreed/features/breeds/data/datasource/api/response/GetAllBreedResponse.kt
@@ -2,9 +2,9 @@ package com.tobioyelekan.dogbreed.features.breeds.data.datasource.api.response
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import com.tobioyelekan.dogbreed.features.breeds.data.datasource.api.model.AllBreed
+import com.tobioyelekan.dogbreed.features.breeds.data.datasource.api.model.AllBreedApiModel
 
 @JsonClass(generateAdapter = true)
 data class GetAllBreedResponse(
-    @Json(name = "message") val message: AllBreed
+    @Json(name = "message") val message: AllBreedApiModel
 )
