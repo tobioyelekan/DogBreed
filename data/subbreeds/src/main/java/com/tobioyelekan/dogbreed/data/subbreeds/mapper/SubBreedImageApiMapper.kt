1201d56d73e09c6e8cb665915157a39d32f078fd
Tobi <miketobi73@gmail.com>
2024-01-31 19:44:23 +0100

    ref: move subbreed mapper to mapper folder

--- data/subbreeds/src/main/java/com/tobioyelekan/dogbreed/data/subbreeds/mapper/SubBreedImageApiMapper.kt
+++ data/subbreeds/src/main/java/com/tobioyelekan/dogbreed/data/subbreeds/mapper/SubBreedImageApiMapper.kt
@@ -1,4 +1,4 @@
-package com.tobioyelekan.dogbreed.data.subbreeds.repository
+package com.tobioyelekan.dogbreed.data.subbreeds.mapper
 
 import com.tobioyelekan.dogbreed.core.model.SubBreedImage
 import com.tobioyelekan.dogbreed.core.network.model.SubBreedImageApiModel
