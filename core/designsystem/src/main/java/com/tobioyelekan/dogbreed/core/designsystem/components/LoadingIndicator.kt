1ed5a6387bdbd0afd9a740cc07935270dcfeea6e
Tobi <miketobi73@gmail.com>
2024-02-02 00:03:05 +0100

    remove unused imports

--- core/designsystem/src/main/java/com/tobioyelekan/dogbreed/core/designsystem/components/LoadingIndicator.kt
+++ core/designsystem/src/main/java/com/tobioyelekan/dogbreed/core/designsystem/components/LoadingIndicator.kt
@@ -8,8 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.semantics
 
 @Composable
 fun LoadingIndicator() {
