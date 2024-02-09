45812a9fa0e23dbb6ec8ad4600ccda5f5d6c0c59
Tobi <miketobi73@gmail.com>
2024-02-09 04:11:55 +0100

    fix: resolve issue of room not emitting updates by keeping same instance of dao objects

--- core/database/src/main/java/com/tobioyelekan/dogbreed/core/database/di/DatabaseModule.kt
+++ core/database/src/main/java/com/tobioyelekan/dogbreed/core/database/di/DatabaseModule.kt
@@ -9,11 +9,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DatabaseModule {
     @Provides
+    @Singleton
     fun providesDatabase(@ApplicationContext context: Context): DogBreedDatabase {
         return Room.databaseBuilder(
             context,
@@ -24,6 +26,7 @@ object DatabaseModule {
     }
 
     @Provides
+    @Singleton
     fun providesBreedsDao(database: DogBreedDatabase): DogBreedDao {
         return database.breedDao()
     }
