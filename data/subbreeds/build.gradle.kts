1989e343f61c48f060503fe75d392f3ed47327b3
Tobi <miketobi73@gmail.com>
2025-09-21 22:42:52 +0100

    migrate :data:subbreeds to use typesafe project accessor

--- data/subbreeds/build.gradle.kts
+++ data/subbreeds/build.gradle.kts
@@ -38,10 +38,10 @@ dependencies {
     implementation(libs.hilt.core)
     ksp(libs.hilt.compiler)
 
-    implementation(project(":core:network"))
-    implementation(project(":core:common"))
-    implementation(project(":core:model"))
+    implementation(projects.core.network)
+    implementation(projects.core.common)
+    implementation(projects.core.model)
 
-    testImplementation(project(":core:testing"))
+    testImplementation(projects.core.testing)
     testImplementation(kotlin("test"))
 }
